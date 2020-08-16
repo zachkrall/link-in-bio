8f365ccd14d02b8f4be9344ec22d4e63e9d56203
Zach Krall <zach@zachkrall.com>
2020-08-16 19:44:48 -0400

    viewport rule

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 const app = document.querySelector("#app")
 
-const linkTemplate = (e, i) => {
+const linkTemplate = (e, i, a) => {
   return `<li><a href="${e.url}" style="background-color:hsl(200,0%, ${
     i * 5 + 10
-  }%);">
+  }%);min-height:${100 / a.length}vh;">
   <h2>${e.title}</h2>
   <p>${e.description}</p>
   <div class="mono">${e.url}</div>
@@ -12,7 +12,7 @@ const linkTemplate = (e, i) => {
 
 const links = [
   {
-    title: "Help a black trans man raise money for safe housing",
+    title: "🆕 Help a black trans man raise money for safe housing",
     description: "",
     url: "https://4asafehaven.glitch.me/",
   },
@@ -22,14 +22,19 @@ const links = [
     url: "https://zachkrall.com",
   },
   {
-    title: "Futureland: zachkrall",
-    description: "Design sketches",
+    title: "Futureland",
+    description: "",
     url: "https://futureland.tv/zachkrall",
   },
   {
-    title: "Zach Krall (@zachkrall) • Instagram",
+    title: "Instagram",
     description: "",
     url: "https://instagram.com/zachkrall",
+  },
+  {
+    title: "Twitter",
+    description: "",
+    url: "https://twitter.com/zachkrall",
   },
 ]
   .map(linkTemplate)
