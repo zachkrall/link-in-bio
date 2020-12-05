662acda294d4776301336a5e7ff891247c9fe0ff
Zach Krall <zach@zachkrall.com>
2020-12-04 21:52:40 -0500

    updates

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 const app = document.querySelector("#app")
 
 const linkTemplate = (e, i, a) => {
-  return `<li><a href="${e.url}" style="background-color:hsl(200,0%, ${
-    i * 5 + 10
-  }%);">
+  return `<li><a href="${e.url}">
   <h2>${e.title}</h2>
   <p>${e.description}</p>
   <div class="mono">${e.url}</div>
@@ -22,34 +20,76 @@ const links = [
     url: "https://no911.nyc/",
   },
   {
-    title: "Portfolio",
+    title: "Anti-Racism",
     description: "",
-    url: "https://zachkrall.com",
-  },
-  {
-    title: "Futureland",
-    description: "",
-    url: "https://futureland.tv/zachkrall",
-  },
-  {
-    title: "Instagram",
-    description: "",
-    url: "https://instagram.com/zachkrall",
-  },
-  {
-    title: "Twitter",
-    description: "",
-    url: "https://twitter.com/zachkrall",
-  },
+    url: "https://omglord.com/anti-racism"
+  }
 ]
   .map(linkTemplate)
   .join("")
 
+let personal_links = [  {
+  title: "Portfolio",
+  description: "",
+  url: "https://zachkrall.com",
+},
+{
+  title: "Futureland",
+  description: "",
+  url: "https://futureland.tv/zachkrall",
+},
+{
+  title: "Instagram",
+  description: "",
+  url: "https://instagram.com/zachkrall",
+},
+{
+  title: "Twitter",
+  description: "",
+  url: "https://twitter.com/zachkrall",
+}]
+.map(linkTemplate)
+.join('')
+
 let html = `
   ${app.innerHTML}
+  <header>
+    <h1>
+      resources
+    </h1>
+  </header>
   <ul>
     ${links}
+  </ul>
+  <header>
+    <h1>
+      social
+    </h1>
+  </header>
+  <ul>
+    ${personal_links}
   </ul>
 `
 
 app.innerHTML = html
+
+
+
+document.body.style.transition = `all ease 0.3s`
+
+const setBackground = () => {
+  document.body.style.background = `
+    radial-gradient(
+      circle at bottom, 
+      hsla(${Math.random() * 360}, 94%, 30%, 1) 0%,
+      hsla(${Math.random() * 360}, 94%, 30%, 1) 50%,
+      hsla(${Math.random() * 360}, 94%, 30%, 1) 100%
+    )
+  `
+}
+
+setBackground();
+
+setInterval(()=>{
+  setBackground()
+}, 2000)
